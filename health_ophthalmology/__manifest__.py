8bc0725e2d71423729932e2de359aa490401d8c3
davidmonterocrespo24 <davidmonterocrespo24>
2020-08-05 16:13:55 -0700

    inicio

---
+++ health_ophthalmology/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'GNU Health ophthalmology package',
+    'summary': '',
+    'description': """GNU Health ophthalmology package
+    """,
+    'version': '1.0.1',
+    'category': 'Hospital Management System',
+    'author': 'Almighty Consulting Solutions Pvt. Ltd.',
+    'support': 'health@gnusolidario.org',
+    'website': 'https://www.gnuhealth.org',
+    'license': 'OPL-1',
+    'depends' : [
+        'base',
+    ],
+    'data' : [
+        'views/oph_form.xml',
+        'views/oph_form.xml',
+        'views/findings_form.xml',
+        'views/findings_tree.xml',
+        'views/health_ophthalmology_view.xml',
+
+    ],
+
+    'application': False,
+    'sequence': 2,
+    'price': 36,
+    'currency': 'EUR',
+}
